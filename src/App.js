b3daa4d61a40d61780c5117a7c14916a3f5a56c5
YuriiMololodtsov <88619986+YuriiMololodtsov@users.noreply.github.com>
2022-10-16 10:37:44 +0300

    new prod

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ToDoItem from './component/ToDo/ToDoItem';
+import './component/ToDo/ToDoItem.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="site__wrapper">
+      <h1>TO DO LIST</h1>
+      <div className="addTask__wrapper">
+        <input placeholder="Введите текст" />
+        <button>Add Task</button>
+      </div>
+      <ToDoItem />
     </div>
   );
 }
---
+++ src/component/ToDo/ToDoItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ToDOItem = (props) => {
+  return (
+    <div className="ToDoItem">
+      <div>Task Item</div>
+      <button>&#10060;</button>
+    </div>
+  );
+};
+
+export default ToDOItem;
