8c830ee3f0557d66a9167baabf07db68c68c47b7
YuriiMololodtsov <88619986+YuriiMololodtsov@users.noreply.github.com>
2022-10-22 08:44:19 +0300

    Added logic React

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import axios from 'axios';
 import ToDoItem from './component/ToDo/ToDoItem';
 import './component/ToDo/ToDoItem.css';
 
 function App() {
+  let [todo, setTodo] = React.useState([]);
+
+  let data = axios.get('http://localhost:8000/');
+  data.then((res) => {
+    console.log(res.data);
+  });
+
   return (
     <div className="site__wrapper">
       <h1>TO DO LIST</h1>
       <div className="addTask__wrapper">
         <input placeholder="Введите текст" />
-        <button>Add Task</button>
+        <button>+</button>
       </div>
       <ToDoItem />
     </div>
